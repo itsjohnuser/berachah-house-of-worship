7a4ec403f2015503b0e112f2c48866b3d239ac11
Santha Kumar Chigurupati <schigurupatis@gmail.com>
2024-11-19 23:01:10 +0530

    worked on slider images fetching

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,29 @@
 import React from "react";
 import Slider from "./Slider"
+import { Slider1, Slider2, Slider3 } from "../utils/constants";
 
 const Home = () => {
     const slidesData = [
         {
             name: "Slider1",
-            url : "https://img.freepik.com/free-photo/3d-music-related-scene_23-2151125037.jpg?t=st=1731569536~exp=1731573136~hmac=b3167e8a5e3a45e40a2ec13de78aca6670ab050a2e5c5ba49891522201bc2383&w=2000",
+            url : Slider1,
         },
         {
             name: "Slider2",
-            url: "https://img.freepik.com/free-photo/purple-hand-background-showing-invisible-object-gesture_53876-104228.jpg?t=st=1731571928~exp=1731575528~hmac=13bebaef9e29453d9e49b389cb88ed3be23fd3f16518abbba1f1c2d594db007f&w=1480",
+            url: Slider2,
         },
         {
             name: "Slider3",
-            url: "https://img.freepik.com/free-vector/realistic-purple-candles-advent-background_23-2149209346.jpg?t=st=1731578062~exp=1731581662~hmac=e2e0a098d669ea953e8456e4033eca9b9aa283288d032c378109586bf89d293b&w=1800",
+            url: Slider3,
         },
         
     ]
+
+    const imageUrls = slidesData.map((slide, index) => slide.url); 
+
   return (
     <>
+              <Slider slides={slidesData} />
         <Slider slides={slidesData} />
         <div className="w-full lg:w-[1024px] mx-auto">
             <div className="about p-5 md:p-2 md:py-7 lg:p-0 lg:py-7">
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,8 @@
 export const bannerimg1 = "../../public/img/banner1.jpg";
 export const bannerimg2 = "./img/banner1.jpg";
+// export const Slider1 = require("../public/img/Slider1.jpg").default;
+// export const Slider2 = require("../../public/img/Slider2.jpg").default;
+// export const Slider3 = require("../../public/img/Slider3.jpg").default;
+export const Slider1 = `${process.env.PUBLIC_URL}/img/Slider1.jpg`;
+export const Slider2 = `${process.env.PUBLIC_URL}/img/Slider2.jpg`;
+export const Slider3 = `${process.env.PUBLIC_URL}/img/Slider3.jpg`;
